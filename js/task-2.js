0e8e67aa5e4d90bd20cccab368962cd6f39fee12
gizemcoban <gizem.coban.r@gmail.com>
2024-08-14 23:53:08 +0300

    task-2 is done

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,21 @@
+class Storage{
+    #items
+    constructor(items){
+        this.#items=items;
+    }
+    getItems(){
+        return this.#items;
+    }
+    addItem(newitem){
+        this.#items.push(newitem);
+    }
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter((item) => item !== itemToRemove);
+      }
+    }
+ 
+
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
